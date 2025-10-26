f30b9f4227f07c45513d82f8ef066185a1c2c3d6
XiaoXhan1234 <3084058513@qq.com>
2025-10-26 10:14:59 +0800

    Update MusicPlayer.svelte

--- src/components/widget/MusicPlayer.svelte
+++ src/components/widget/MusicPlayer.svelte
@@ -18,7 +18,7 @@ let meting_api =
 	musicPlayerConfig.meting_api ??
 	"https://www.bilibili.uno/api?server=:server&type=:type&id=:id&auth=:auth&r=:r";
 // Meting API 的 ID，从配置中获取或使用默认值
-let meting_id = musicPlayerConfig.id ?? "14164869977";
+let meting_id = musicPlayerConfig.id ?? "7452421335";
 // Meting API 的服务器，从配置中获取或使用默认值,有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
 let meting_server = musicPlayerConfig.server ?? "netease";
 // Meting API 的类型，从配置中获取或使用默认值
