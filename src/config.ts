ee3cc642e8816d4dd008a6144e239352cbb6538f
XiaoXhan1234 <3084058513@qq.com>
2025-11-09 10:37:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -244,18 +244,17 @@ export const navBarConfig: NavBarConfig = {
                 },
 			],
 		},
-		// 新增的独立统计菜单项
-		{
-			name: "统计",
-			url: "https://cloud.umami.is/share/fcdazKOajfiuNUzO",
-			icon: "material-symbols:analytics", // 使用统计相关的图标
-			external: true,
-		},
 		{
 			name: "关于",
 			url: "/content/",
 			icon: "material-symbols:info",
-			children: [LinkPreset.About, LinkPreset.Friends],
+			children: [LinkPreset.About, LinkPreset.Friends,
+			{
+      name: '统计',
+      url: 'https://cloud.umami.is/share/fcdazKOajfiuNUzO',     
+      external: true,                               
+                },
+			],
 		},
 	],
 };
