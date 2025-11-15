a356026565f60817f448af7af28cb5a695dff29f
xiaoxhan1234 <3084058513@qq.com>
2025-11-16 05:21:41 +0800

    Update 1 file: config.ts

--- src/config.ts
+++ src/config.ts
@@ -475,7 +475,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 移动端断点：屏幕宽度小于768px
 			mobile: 768,
 			// 平板端断点：屏幕宽度小于1280px
-			tablet: 1280,
+			tablet: 1024,
 			// 桌面端断点：屏幕宽度小于1280px
 			desktop: 1280,
 		},
