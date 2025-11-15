d4c7fe8a2b982ea94821b8aadb13c6b0d49f8637
xiaoxhan1234 <3084058513@qq.com>
2025-11-15 22:31:41 +0000

    Deploying to pages from @ xiaoxhan1234/Xhani-Blog@68dc7575307d73ed0621912c95612316cc24e4b4 🚀

---
+++ _astro/url-utils.umSobhQX.js
@@ -0,0 +1,1 @@
+import"./config.DAQS4Fa6.js";import"./zh_TW.DrrWwPS4.js";function u(o,t){const n=o.replace(/^\/|\/$/g,"").toLowerCase(),r=t.replace(/^\/|\/$/g,"").toLowerCase();return n===r}function e(...o){return o.join("/").replace(/\/+/g,"/")}function c(o){return a(`/posts/${o}/`)}function a(o){return e("","/",o)}export{c as g,u as p,a as u};
