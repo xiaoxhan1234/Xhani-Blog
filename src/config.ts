7049b392504d1a4d4d034ae8cd2e942d236ed0c1
xiaoxhan1234 <3084058513@qq.com>
2025-11-16 12:24:55 +0800

    Update 1 file: config.ts

--- src/config.ts
+++ src/config.ts
@@ -128,7 +128,7 @@ export const siteConfig: SiteConfig = {
 		carousel: {
 			enable: true, // 为 true 时：为多张图片启用轮播。为 false 时：从数组中随机显示一张图片
 
-			interval: 1.5, // 轮播间隔时间（秒）
+			interval: 2, // 轮播间隔时间（秒）
 		},
 
 		waves: {
@@ -242,7 +242,7 @@ export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 		interval: 5, // 轮播间隔时间（秒）
 	},
 	zIndex: -1, // 层级，确保壁纸在背景层
-	opacity: 0.8, // 壁纸透明度
+	opacity: 5, // 壁纸透明度
 	blur: 1, // 背景模糊程度
 };
 
