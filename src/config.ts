ddbd0d1ba5800daad0fff27dce2a0646d3e1ceac
xiaoxhan1234 <3084058513@qq.com>
2026-02-20 09:29:05 +0800

    Update 5 files: model.json, model.moc, model.moc3, default-costume.png, config.ts

--- src/config.ts
+++ src/config.ts
@@ -617,7 +617,7 @@ export const sakuraConfig: SakuraConfig = {
 
 // Pio 看板娘配置
 export const pioConfig: import("./types/config").PioConfig = {
-	enable: true, // 启用看板娘
+	enable: false, // 启用看板娘
 	models: ["/pio/models/pio/model.json"], // 默认模型路径
 	position: "left", // 模型位置
 	width: 280, // 默认宽度
